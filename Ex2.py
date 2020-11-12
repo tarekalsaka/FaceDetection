b3aa478cf0d298a5b552863c62d27411e226556e
francescobrusca <73594409+francescobrusca@users.noreply.github.com>
2020-11-12 19:53:03 +0100

    ex2_lab3_finished

--- Ex2.py
+++ Ex2.py
@@ -135,119 +135,143 @@ model = load_model('my_model_trained.h5')
 model.summary()
 # %%    
 import time
-
-
-Window_size = [16,19,23,28,33,40,48,57,69,83,99]
-time_window = []
-precision_evolution = []
-recall_evolution = []
-F1score_evolution =  []
-for w in Window_size:
-    start = time.time()
-    image_path, ann= readTextFile(annotation_path)
-    choose_image_to_work_on=image_path
-    precision_window = []
-    recall_window = []
-    F1score_window = []
-    for index,path_list in enumerate(choose_image_to_work_on): #iteration over the images
-        if index % 50 == 0:
-            print("image I work on ",index)
-        faces, numFace,fullPath= parseAnnotation(path_list,ann)
-        tmp_img = cv2.imread(fullPath)
-        tmp_img = cv2.cvtColor(tmp_img, cv2.COLOR_RGB2GRAY) # put image in gray scale bc the network is trained in gray scale
-#        plt.figure()
-#        plt.imshow(tmp_img, cmap='gray') 
-       
-        #next reshaping is to add the channel value (1)
-        tmp_img = tmp_img.reshape(tmp_img.shape[0], tmp_img.shape[1],1)
-        
-        
-        crop_faces, coord_crop_faces = crop_face(fullPath,faces,numFace)
-        
-        dict_faces = {}   #initialization of a dictionary to understand which and how many faces will be recognised 
-        for i in range(len(faces)):
-            dict_faces[i] = 0
-            
-    #    print('inizializzazione del dizionario:', dict_faces)
-        
-        windowSize = w
-        TP = 0 #true positive
-        FP = 0 #false positive
-        TN = 0 #true negative
-        FN = 0 #false negative
-        n_detected_faces = 0
-        detected_faces = []
-        crop_faces_corresponding = []
-        false_negative = []
-        missing_face = 0
-        for (x, y, window) in sliding_window(tmp_img, stepSize=int(windowSize/4), windowSize=(windowSize,windowSize)):
-            #reshaping the window for go in the network..
-            window = cv2.resize(window, (24,16))
-            window = window.reshape(1, window.shape[0], window.shape[1], 1)
-            y_pred = model.predict(window)
-            y_pred = np.argmax(y_pred, axis=1)
-    #        n_faces = 0
-            
-            '''
-                TRUE POSITIVE: 
-                    window detect a face (y_pred == 0) and was a face (iou > 0.5)
-                FALSE POSITIVE:
-                    window detect a face (y_pred == 0) and was not a face (iou < 0.5)
-                TRUE NEGATIVE: 
-                    window detect a background (y_pred == 1) and was a background (iou < 0.5)
-                FALSE NEGATIVE: 
-                    window detect a background (y_pred == 1) and was a face (iou > 0.5)
-            '''
-            window_coord = x,y,x + windowSize, y + windowSize
+run_algorithm= False
+if run_algorithm:
     
-            if (y_pred == 0): #the model predict that in the window there is a face
-                for i in range(len(coord_crop_faces)):
-                    iou = bb_intersection_over_union(coord_crop_faces[i], window_coord)
-                    if iou > 0.5: #true positive
-                        TP +=1
-                        #distinguere le facce!!!!!!!!!
-                        if dict_faces[i] == 0: #the face reconised from the model is memorized if before wasn't
-#                            print('trovata una')
-                            dict_faces[i] = 1
-#                            n_detected_faces += 1
-#                            detected_faces.append(window)
-#                            crop_faces_corresponding.append(crop_faces[i])
-                        #in case not if that face was already recognised
-    
-                    else:         #false positive
-                        FP +=1
-            else: #y_pred==1, the model predict that in the window there is a background
-                for i in range(len(coord_crop_faces)):
-                    iou = bb_intersection_over_union(coord_crop_faces[i], window_coord)
-                    if iou > 0.5: #false negative
-                        FN +=1
-#                        false_negative.append(window)
-#                        missing_face +=1
-                    else:         #true negative
-                        TN +=1
-#        print('the number of the faces in the original image was ', numFace)
-#        print('the model found ', n_detected_faces, ' faces')
-#    
-#        print('the faces detected were:')
-#        for i in range(len(detected_faces)):
-#            plt.figure()
-#            plt.imshow(np.squeeze(detected_faces[i]), cmap='gray')
-#            plt.figure()
-#            plt.imshow(crop_faces_corresponding[i], cmap ='gray')
-#           
-#        print(TP)
-#        print(FP)
-#        print(TN)
-#        print(FN)
-        #  Report precision, recall, F1 and
-        precision_tmp_img = check_division_by0(TP,(TP+FP))
-        recall_tmp_img = check_division_by0(TP,(TP+FN))
-        F1score_tmp_img = check_division_by0((2*(precision_tmp_img*recall_tmp_img)),(precision_tmp_img+recall_tmp_img))
-        precision_window.append(precision_tmp_img)
-        recall_window.append(recall_tmp_img)
-        F1score_window.append(F1score_tmp_img)
+    Window_size = [16,19,23,28,33,40,48,57,69,83,99]
+    time_window = []
+    precision_evolution = []
+    recall_evolution = []
+    F1score_evolution =  []
+    for w in Window_size:
+        start = time.time()
+        image_path, ann= readTextFile(annotation_path)
+        choose_image_to_work_on=image_path
+        precision_window = []
+        recall_window = []
+        F1score_window = []
+        for index,path_list in enumerate(choose_image_to_work_on): #iteration over the images
+            if index % 50 == 0:
+                print("image I work on ",index)
+            faces, numFace,fullPath= parseAnnotation(path_list,ann)
+            tmp_img = cv2.imread(fullPath)
+            tmp_img = cv2.cvtColor(tmp_img, cv2.COLOR_RGB2GRAY) # put image in gray scale bc the network is trained in gray scale
+    #        plt.figure()
+    #        plt.imshow(tmp_img, cmap='gray') 
+           
+            #next reshaping is to add the channel value (1)
+            tmp_img = tmp_img.reshape(tmp_img.shape[0], tmp_img.shape[1],1)
+            
+            
+            crop_faces, coord_crop_faces = crop_face(fullPath,faces,numFace)
+            
+            dict_faces = {}   #initialization of a dictionary to understand which and how many faces will be recognised 
+            for i in range(len(faces)):
+                dict_faces[i] = 0
+                
+        #    print('inizializzazione del dizionario:', dict_faces)
+            
+            windowSize = w
+            TP = 0 #true positive
+            FP = 0 #false positive
+            TN = 0 #true negative
+            FN = 0 #false negative
+            n_detected_faces = 0
+            detected_faces = []
+            crop_faces_corresponding = []
+            false_negative = []
+            missing_face = 0
+            for (x, y, window) in sliding_window(tmp_img, stepSize=int(windowSize/2), windowSize=(windowSize,windowSize)):
+                #reshaping the window for go in the network..
+                window = cv2.resize(window, (24,16))
+                window = window.reshape(1, window.shape[0], window.shape[1], 1)
+                y_pred = model.predict(window)
+                y_pred = np.argmax(y_pred, axis=1)
+        #        n_faces = 0
+                
+                '''
+                    TRUE POSITIVE: 
+                        window detect a face (y_pred == 0) and was a face (iou > 0.5)
+                    FALSE POSITIVE:
+                        window detect a face (y_pred == 0) and was not a face (iou < 0.5)
+                    TRUE NEGATIVE: 
+                        window detect a background (y_pred == 1) and was a background (iou < 0.5)
+                    FALSE NEGATIVE: 
+                        window detect a background (y_pred == 1) and was a face (iou > 0.5)
+                '''
+                window_coord = x,y,x + windowSize, y + windowSize
         
-    precision_evolution.append(np.mean(precision_window))  
-    recall_evolution.append(np.mean(recall_window))
-    F1score_evolution.append(np.mean(F1score_window))
-time_window.append(time.time()-start)
+                if (y_pred == 0): #the model predict that in the window there is a face
+                    for i in range(len(coord_crop_faces)):
+                        iou = bb_intersection_over_union(coord_crop_faces[i], window_coord)
+                        if iou > 0.5: #true positive
+                            TP +=1
+                            #distinguere le facce!!!!!!!!!
+                            if dict_faces[i] == 0: #the face reconised from the model is memorized if before wasn't
+    #                            print('trovata una')
+                                dict_faces[i] = 1
+    #                            n_detected_faces += 1
+    #                            detected_faces.append(window)
+    #                            crop_faces_corresponding.append(crop_faces[i])
+                            #in case not if that face was already recognised
+        
+                        else:         #false positive
+                            FP +=1
+                else: #y_pred==1, the model predict that in the window there is a background
+                    for i in range(len(coord_crop_faces)):
+                        iou = bb_intersection_over_union(coord_crop_faces[i], window_coord)
+                        if iou > 0.5: #false negative
+                            FN +=1
+    #                        false_negative.append(window)
+    #                        missing_face +=1
+                        else:         #true negative
+                            TN +=1
+    #        print('the number of the faces in the original image was ', numFace)
+    #        print('the model found ', n_detected_faces, ' faces')
+    #    
+    #        print('the faces detected were:')
+    #        for i in range(len(detected_faces)):
+    #            plt.figure()
+    #            plt.imshow(np.squeeze(detected_faces[i]), cmap='gray')
+    #            plt.figure()
+    #            plt.imshow(crop_faces_corresponding[i], cmap ='gray')
+    #           
+    #        print(TP)
+    #        print(FP)
+    #        print(TN)
+    #        print(FN)
+            #  Report precision, recall, F1 and
+            precision_tmp_img = check_division_by0(TP,(TP+FP))
+            recall_tmp_img = check_division_by0(TP,(TP+FN))
+            F1score_tmp_img = check_division_by0((2*(precision_tmp_img*recall_tmp_img)),(precision_tmp_img+recall_tmp_img))
+            precision_window.append(precision_tmp_img)
+            recall_window.append(recall_tmp_img)
+            F1score_window.append(F1score_tmp_img)
+            
+        precision_evolution.append(np.mean(precision_window))  
+        recall_evolution.append(np.mean(recall_window))
+        F1score_evolution.append(np.mean(F1score_window))
+    time_window.append(time.time()-start)
+
+# %%
+import pickle
+#with open("precision_evolution_Ex2.txt", "wb") as fp:   
+#    pickle.dump(precision_evolution, fp)
+#with open("recall_evolution_Ex2.txt", "wb") as fp:   
+#    pickle.dump(recall_evolution, fp)
+#with open("F1score_evolution_Ex2.txt", "wb") as fp:   
+#    pickle.dump(F1score_evolution, fp)    
+with open("F1score_evaluation_Ex2.txt", "rb") as fp:   # Unpickling
+    b = pickle.load(fp)   
+with open("precision_evaluation_Ex2.txt", "rb") as fp:   # Unpickling
+    b = pickle.load(fp)
+with open("recall_evaluation_Ex2.txt", "rb") as fp:   # Unpickling
+    b = pickle.load(fp)
+ 
+plt.figure()
+plt.plot(F1score_evolution, 'r', label='f1 score' )
+plt.plot(precision_evolution, color='orange', linestyle='--', label= 'precision')
+plt.plot(recall_evolution, 'g--', label='recall')
+plt.xlabel('Different window size (11) with scale factor=1.2')
+plt.legend()
+    
